a1232fb0e3d0f1597719e2d8d3f40309b04aefb6
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2024-11-01 00:56:08 -0300

    Cambiar el color primario

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#EBF0FF",
+          100: "#D6E0FF",
+          200: "#A9BEFF",
+          300: "#80A0FE",
+          400: "#527DFE",
+          500: "#285FFE",
+          600: "#013BEA",
+          700: "#012DB2",
+          800: "#011E75",
+          900: "#000F3D",
+          950: "#00081E",
+        },
+      },
+    },
   },
   plugins: [],
 };
