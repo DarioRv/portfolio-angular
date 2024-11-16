581dd7d8ea9178ff11b9c7b023a4bfa8ab631eae
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2024-11-15 22:51:25 -0300

    Añadir funcionalidad al formulario

--- src/app/portfolio/pages/contact/contact.component.ts
+++ src/app/portfolio/pages/contact/contact.component.ts
@@ -5,6 +5,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
@@ -15,6 +16,7 @@ import {
 })
 export class ContactComponent {
   private readonly fb = inject(FormBuilder);
+  private readonly router = inject(Router);
   public contactForm = this.fb.group({
     name: ['', [Validators.required]],
     email: ['', [Validators.required, Validators.email]],
@@ -28,6 +30,9 @@ export class ContactComponent {
     ],
   });
 
+  success: boolean = false;
+  error: boolean = false;
+
   get name(): FormControl {
     return this.contactForm.get('name') as FormControl;
   }
@@ -40,10 +45,43 @@ export class ContactComponent {
     return this.contactForm.get('message') as FormControl;
   }
 
-  onSubmit(): void {
+  onSubmit(event: Event): void {
+    event.preventDefault();
     if (this.contactForm.invalid) {
       this.contactForm.markAllAsTouched();
       return;
     }
+
+    const formData = new URLSearchParams();
+    formData.append('name', this.name.value);
+    formData.append('email', this.email.value);
+    formData.append('message', this.message.value);
+
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: formData,
+    })
+      .then(() => this.showSuccessMessage())
+      .catch((error) => this.showErrorMessage());
+  }
+
+  showSuccessMessage(): void {
+    this.success = true;
+    setTimeout(() => {
+      this.success = false;
+      this.contactForm.reset({
+        email: '',
+        name: '',
+        message: '',
+      });
+    }, 5000);
+  }
+
+  showErrorMessage(): void {
+    this.error = true;
+    setTimeout(() => {
+      this.error = false;
+    }, 5000);
   }
 }
