b38b848fe596c15fb84b7d18fc230fe9316ea4d3
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2024-11-17 19:29:19 -0300

    Agregar animaciones

--- src/app/portfolio/pages/contact/contact.component.ts
+++ src/app/portfolio/pages/contact/contact.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import cv from '@data/cv.json';
 import { IconPipe } from '@pipes/icon.pipe';
+import { DecorationComponent } from '../../components/decoration/decoration.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [IconPipe, MatIcon],
+  imports: [IconPipe, MatIcon, DecorationComponent],
   templateUrl: './contact.component.html',
   styles: ``,
 })
