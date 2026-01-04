cda55c4e51a549ba23bad0e2b42fa9f7cefe0327
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2026-01-04 17:46:05 -0300

    Rediseño de sección experiencia

--- src/app/portfolio/components/tech-chip/tech-chip.component.ts
+++ src/app/portfolio/components/tech-chip/tech-chip.component.ts
@@ -1,4 +1,4 @@
-import { NgStyle } from '@angular/common';
+import { NgClass } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { Technology } from '@interfaces/project.interface';
 import { IconPipe } from '@pipes/icon.pipe';
@@ -6,10 +6,11 @@ import { IconPipe } from '@pipes/icon.pipe';
 @Component({
   selector: 'tech-chip',
   standalone: true,
-  imports: [NgStyle, IconPipe],
+  imports: [NgClass, IconPipe],
   templateUrl: './tech-chip.component.html',
   styles: ``,
 })
 export class TechChipComponent {
   public tech = input.required<Technology>();
+  public styleClass = input<string>();
 }
